cf04af4cb7adc4d64486d297a30e151a456b4e96
netodiegues <netodiegues.nd@gmail.com>
2018-07-14 16:51:46 -0300

    Fix

--- src/busstation/BusStationGFT.java
+++ src/busstation/BusStationGFT.java
@@ -2,7 +2,6 @@ package busstation;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
